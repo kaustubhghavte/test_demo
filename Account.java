0aae1b2cf038c40cb95242f8cf32a16c9d76af7e
kaustubhghavte <57046546+kaustubhghavte@users.noreply.github.com>
2022-08-23 20:08:44 +0530

    version0.0

---
+++ Account.java
@@ -0,0 +1,27 @@
+public class Account{
+	public static void main(String[] args){
+		
+		String str = "MADAM";
+		
+		int start = 0;//0,1,2
+		int end = str.length()-1;//4,3,2
+		boolean flag = true;
+		
+		while(end>start){
+			if(str.charAt(start) != str.charAt(end)){
+				flag = false;
+				break;
+			}
+			start++;
+			end--;
+		}
+		
+		if(flag){
+			System.out.println("Palimdrom");
+		}else{
+			System.out.println("NOT-Palimdrom");
+		}
+	
+	}
+}
+
